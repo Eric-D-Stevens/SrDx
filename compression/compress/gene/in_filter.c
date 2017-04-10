397a6f23684b936c67b925c498495604ba078530
Eric <Eric@Loris-MacBook-Pro.local>
2017-04-10 02:11:13 -0700

    working

--- compression/compress/gene/in_filter.c
+++ compression/compress/gene/in_filter.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 
-int main(int argc, char argv[]){
+int main(int argc, char ** argv){
 	
 	// Input output file pointers
 	FILE * fp_in;
@@ -12,8 +12,8 @@ int main(int argc, char argv[]){
 	
 
 	// Open files	
-	fp_in = fopen( "../processing/gene.tst" , "r" );
-	fp_out = fopen( "something.gc" , "w" );
+	fp_in = fopen( "./gene.tst" , "r" );
+	fp_out = fopen( "compressed.gc" , "w" );
 	
 	// Compression characters
 	int big[4];
@@ -24,13 +24,12 @@ int main(int argc, char argv[]){
 	int *sp =  &small;
 
 	big[0]=fgetc(fp_in);		
-	printf("\n %c = %x", big[0], (int)big[0]);
-	while(big[0] != EOF){
+	
+	while(!feof(fp_in)){
 		// check for new line character
 		if(big[0] == '\n'){
-			//fputc((int)big[0],fp_out); // rm 4 spa eff
 			big[0]=fgetc(fp_in);
-		}	
+		}
 		// Load buffer 'big'
 		big[1]=fgetc(fp_in);		
 		big[2]=fgetc(fp_in);		
@@ -45,6 +44,7 @@ int main(int argc, char argv[]){
 		// combine into single char
 		*sp = *(bp+0) | *(bp+1) | *(bp+2) | *(bp+3);
 
+		if(feof(fp_in)){break;}
 		// put compressed char into output file	
 		fputc((int)small, fp_out);
 		
@@ -55,7 +55,7 @@ int main(int argc, char argv[]){
 	}
 	
 	// end output file
-	fputc((int)(EOF), fp_out);
+	//fputc((int)(EOF), fp_out);
 	fclose(fp_in);
 	fclose(fp_out);
 }
--- compression/decompress/gene/out_filter.c
+++ compression/decompress/gene/out_filter.c
@@ -6,7 +6,7 @@
 #define FILE_SIZE 100000000
 
 
-int main(int argc, char argv[]){
+int main(int argc, char ** argv){
 	
 	// Input output file pointers
 	FILE * fp_in;
@@ -15,8 +15,8 @@ int main(int argc, char argv[]){
 	
 
 	// Open files	
-	fp_in = fopen( "something.gc" , "r" );
-	fp_out = fopen( "decomp.gene" , "w" );
+	fp_in = fopen( "compressed.gc" , "r" );
+	fp_out = fopen( "gene.tst" , "w" );
 	
 	// Compression characters
 	int b[4];
@@ -25,31 +25,46 @@ int main(int argc, char argv[]){
 	uint64_t fcount=1;	
 	int count;
 		
-	while(fcount < FILE_SIZE){
+	s = fgetc(fp_in);
+	while(s != EOF){
+		if(feof(fp_in)){break;}
 		for(count=0; count<(LINE_SIZE/4); count++){
 
 			// pull char into s
-			s = fgetc(fp_in);
 			
 			// split
+			if(s==EOF){break;}
 			b[0]=((s & 0x03)<<1)|0x41;
 			b[1]=((s & 0x0c)>>1)|0x41;
 			b[2]=((s & 0x30)>>3)|0x41;
 			b[3]=((s & 0xc0)>>5)|0x41;
 
+			if(feof(fp_in)){break;}
+			
+			// replace e with t
+			int t_search;
+			for(t_search=0; t_search<4; t_search++){
+				if(b[t_search]=='E'){b[t_search]='T';}
+			}
+			
 			// write chars
 			fputc(b[3],fp_out);
 			fputc(b[2],fp_out);
 			fputc(b[1],fp_out);
 			fputc(b[0],fp_out);
+			
+			
+			printf("%c%c%c%c",b[3],b[2],b[1],b[0]);
+		
 
-			fcount=fcount+1;	
-
+			s = fgetc(fp_in);
+			if(feof(fp_in)){break;}
 		}
+		
+		printf("\n");
 		fputc('\n',fp_out);
 	}
 
-	fputc(EOF, fp_out);
 	fclose(fp_in);
 	fclose(fp_out);
 }
